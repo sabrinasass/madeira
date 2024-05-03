1dcaa775d8273be68327496ced66448f2b09392f
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-05-03 14:15:37 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -1,52 +1,16 @@
-# myFirstStreamlitApp.py
-#import the libraries
 import streamlit as st
-from PIL import Image
+import pandas as pd
+from Massaki01 import *
 
-image01 = Image.open('panda.jpg')
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Bem vindo, EMEF/EJA Edson Luis Lima Souto!")
-
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
-
-# Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-
-st.subheader("------ **Desenvolvido por: Massaki de O. Igarashi** -----")
-
-menu = ["Texto_Colunas",
-        "Texto_Markdown",
-        "Inserir_Figura"]
-choice = st.sidebar.selectbox("Menu de Opções",menu)
-st.sidebar.write("Texto Side Bar")
+Indice()
     
-if choice == "Texto_Colunas":       
-    st.subheader("Texto formatado em colunas")
-    st.write("Veja a seguir uma opção de formatação em colunas")    
-    cols01 = st.columns(2)    
-    cols01[0].write('Texto da Coluna 01')
-    cols01[1].write('Texto da Coluna 02')
-elif choice == "Texto_Markdown":
-    st.subheader("Texto Markdown")
-    st.write("Veja a seguir opção de formatação de texto Markdown")
-    st.markdown(
-    """
-    ## *Alguns sites referências*:    
-    - [Streamlit: hello world](https://calmcode.io/streamlit/hello-world.html)
-    - [:star: Streamlit emoji shortcodes](https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlitapp.com/)
-    - [Layouts and Containers](https://docs.streamlit.io/library/api-reference/layout)
+ MKD(Versao(), 'left', 18, 'blue')
    
-    ##### CRONOGRAMA
-    DIA | CH HORÁRIA | CONTEÚDO
-    :---------: | :------: | :-------:
-    Dia 1 de 2 | ?h | ? a ?
-    Dia 2 de 2 | ?h | ? a ?
-    """
-    )
-elif choice == "Inserir_Figura":
-    st.image(image01, width=800, caption='Rótulo da Imagem 01') 
-    
+MKD('⬜ Barra_Lateral_Texto', 'left', 48, 'darkblue')
+   
+Escrever('Esta função permite adicionar alguns elementos de texto com estilos específicos na Barra Lateral.', 'subcabecalho')
+Escrever(' ')
+Escrever('Observação: ', 'subcabecalho')
+CONTAINER = Container(True)
+with CONTAINER: 
+        MKD('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessa leiga na Programação.', "justified", 20, "black")
