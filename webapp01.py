61c95de3a24e1d72ca09ac4dc654d9bc1806ed42
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-05-03 14:18:55 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -4,9 +4,9 @@ from Massaki01 import *
 
 MKD(Versao(), 'left', 18, 'blue')
    
-MKD('⬜ Massaki01 - Biblioteca teste 01', 'left', 48, 'darkblue')
+MKD('⬜ Massaki01 - Biblioteca teste', 'left', 48, 'darkblue')
    
-Escrever('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessa leiga na Programação.', 'subcabecalho')
+Escrever('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessoa leiga na Programação.', 'subcabecalho')
 Escrever(' ')
 Escrever('Observação: Em desenvolvimento!', 'subcabecalho')
 CONTAINER = Container(True)
