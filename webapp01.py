af33d5fb04784446daacfd5798b616940c78ac26
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-05-03 14:21:32 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -2,13 +2,40 @@ import streamlit as st
 import pandas as pd
 from Massaki01 import *
 
-MKD(Versao(), 'left', 18, 'blue')
-   
-MKD('⬜ Massaki01 - Biblioteca teste', 'left', 48, 'darkblue')
-   
-Escrever('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessoa leiga na Programação.', 'subcabecalho')
-Escrever(' ')
-Escrever('Observação: Em desenvolvimento!', 'subcabecalho')
-CONTAINER = Container(True)
-with CONTAINER: 
-        MKD('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessa leiga na Programação.', "justified", 20, "black")
+def main():
+   Configurar_Pagina("ACT - Soluções para Pessoas/TESTE", "amplo", "auto", "https://docs.streamlit.io", "mailto:informacoes.actsp@gmail.com", "#### **ACT - Soluções para Pessoas**. Você pode usar formatação Markdown para adicionar informações neste espaço. Para mais informações acesse *https://www.markdownguide.org*", "	©️")
+  
+   MKD(Versao(), 'left', 18, 'blue')
+      
+   MKD('⬜ Massaki01 - Biblioteca teste', 'left', 48, 'darkblue')
+      
+   Escrever('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessoa leiga na Programação.', 'subcabecalho')
+   Escrever(' ')
+   Escrever('Observação: Em desenvolvimento!', 'subcabecalho')
+   CONTAINER = Container(True)
+   with CONTAINER: 
+      MKD('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessa leiga na Programação.', "justified", 20, "black")
+    
+   Barra_Lateral_Texto("Desenvolvido por Massaki Igarashi")
+
+    colunas = Colunas(4)       
+    
+    with colunas[0]:
+        BTN1 = Botao_M("✔️ Botão 1")
+    with colunas[1]:
+        BTN2 = Botao_C("✔️ Botão 2", "gray")
+    with colunas[2]:
+        BTN3 = Botao_M("Botão 3")
+    with colunas[3]:   
+        BTN4 = Botao_C("✔️ Botão 4")
+       
+    if BTN1:
+        Escrever("Você clicou no Botão 1")
+    if BTN2:
+        Escrever("Você clicou no Botão 2")
+    if BTN3:
+        Escrever("Você clicou no Botão 3")       
+    if BTN4:
+        Escrever("Você clicou no Botão 4") 
+if __name__ == '__main__':
+	main()
